969c94ebf0e5845b335793c71f466b2b596f1354
franziska.haaf <haaf@mailbox.org>
2022-12-12 16:31:41 +0100

    Creating a basic text in figma and zooming on it after creation

--- code.ts
+++ code.ts
@@ -1,11 +1,26 @@
 figma.showUI(__html__);
 
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = async msg => {
     if (msg.type === 'generate- name') {
         console.log("Gender is: ", msg.gender)
         console.log("First name used: ", msg.hasFirstName)
         console.log("Last name used: ", msg.hasLastName)
 
+        /**
+         * Generate the figma text object and wrap it in a node, so we can zoom on it later
+         */
+        const nodes: SceneNode[] = []
+        const name = figma.createText()
+        figma.currentPage.appendChild(name)
+        nodes.push(name)
+        // Load the font in the text node before setting the character
+        // @ts-ignore
+        await figma.loadFontAsync(name.fontName)
+
+        /**
+         * Build the actual name
+         */
+        name.characters = msg.gender + msg.hasFirstName + msg.hasLastName
 
         /*const nodes: SceneNode[] = [];
         for (let i = 0; i < msg.count; i++) {
@@ -20,6 +35,11 @@ figma.ui.onmessage = msg => {
         }
         figma.currentPage.selection = nodes;
         figma.viewport.scrollAndZoomIntoView(nodes);*/
+        /**
+         * Zoom on the created name
+         */
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
     }
     if (msg.type === 'close') {
         figma.closePlugin();
